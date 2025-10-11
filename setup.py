74b09166a1ee1446379678a70b6de0f077783e26
Leonardo Nobre <60514067+NobreHD@users.noreply.github.com>
2025-10-11 19:12:05 +0100

    Bye Hatch

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, Extension
+import sys
+
+extra_compile_args = []
+if sys.platform != 'win32':
+    extra_compile_args = ['-O3', '-Wall']
+else:
+    extra_compile_args = ['/O2']
+
+setup(
+    ext_modules=[
+        Extension(
+            'cijak._native',
+            sources=['src/cijak/_native.c'],
+            extra_compile_args=extra_compile_args,
+            optional=True,  # Build won't fail if C compilation fails
+        )
+    ],
+)
