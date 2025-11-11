e4c737ef31b76426d7a260cab8f66a38ef90293e
Leonardo Nobre <60514067+NobreHD@users.noreply.github.com>
2025-11-11 23:50:40 +0000

    Change version

--- src/cijak/__init__.py
+++ src/cijak/__init__.py
@@ -1,9 +1,9 @@
 try:
   from cijak._native import Cijak
-  __version__ = "1.0.0+native"
+  __version__ = "1.0.1+native"
 except ImportError:
   from cijak._fallback import Cijak
-  __version__ = "1.0.0+fallback"
+  __version__ = "1.0.1+fallback"
   import warnings
   warnings.warn("Using pure Python fallback (300x slower). Install build tools for C extension.")
 
